6ea610fe7da8e624eb9e90bbf7ddfedb41f4e77a
JeongHeumChoi <andyc707@gmail.com>
2023-11-20 05:21:43 +0900

    feat: route 처리

--- pathfinder-front/src/pages/index.jsx
+++ pathfinder-front/src/pages/index.jsx
@@ -1,12 +1,21 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom";
+
 import AppLayout from "../components/AppLayout";
+// import RtImageList from "./RtImageList";
+import UserRegister from "./accounts/UserRegister";
+import UserLogin from "./accounts/UserLogin";
 
 function Root() {
     return (
         <AppLayout>
-            최상위 컴포넌트
+            <Routes>
+                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/registration" element={<UserRegister />} />
+                <Route path="/login" element={<UserLogin />} />
+                {/* 기타 라우트 */}
+            </Routes>
         </AppLayout>
-
     );
 }
 
