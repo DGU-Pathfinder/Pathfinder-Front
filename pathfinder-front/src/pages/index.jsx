3afea2029b53caf78fe79ef136631c87e5d01c6b
JeongHeumChoi <andyc707@gmail.com>
2023-11-20 14:19:14 +0900

    feat: RtImage 컴포넌트 연결

--- pathfinder-front/src/pages/index.jsx
+++ pathfinder-front/src/pages/index.jsx
@@ -5,12 +5,13 @@ import AppLayout from "../components/AppLayout";
 // import RtImageList from "./RtImageList";
 import UserRegister from "./accounts/UserRegister";
 import UserLogin from "./accounts/UserLogin";
+import RtImageList from "./RtImageList";
 
 function Root() {
     return (
         <AppLayout>
             <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/" element={<RtImageList />} />
                 <Route path="/registration" element={<UserRegister />} />
                 <Route path="/login" element={<UserLogin />} />
                 {/* 기타 라우트 */}
