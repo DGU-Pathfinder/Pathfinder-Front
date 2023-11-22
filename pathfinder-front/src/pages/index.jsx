46b8ab2f0b1c00578ec390c9a12605df7dfb3181
JeongHeumChoi <andyc707@gmail.com>
2023-11-23 01:40:16 +0900

    feat: 이미지 업로드 경로 추가

--- pathfinder-front/src/pages/index.jsx
+++ pathfinder-front/src/pages/index.jsx
@@ -1,20 +1,20 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom";
-
+import { ConfigProvider } from "antd";
 import AppLayout from "../components/AppLayout";
-// import RtImageList from "./RtImageList";
 import UserRegister from "./accounts/UserRegister";
 import UserLogin from "./accounts/UserLogin";
 import RtImageList from "./RtImageList";
+import UploadImage from "./UploadImage";
 
 function Root() {
     return (
         <AppLayout>
             <Routes>
                 <Route path="/" element={<RtImageList />} />
+                <Route path="/upload-image" element={<UploadImage />} />
                 <Route path="/registration" element={<UserRegister />} />
                 <Route path="/login" element={<UserLogin />} />
-                {/* 기타 라우트 */}
             </Routes>
         </AppLayout>
     );
