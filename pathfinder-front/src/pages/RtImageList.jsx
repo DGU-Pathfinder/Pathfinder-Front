47caa4558973f4a91158e30264a5b0f70b550d5f
JeongHeumChoi <andyc707@gmail.com>
2023-11-24 15:24:32 +0900

    feat: Pagination 추가 및 RtImage grid처리

--- pathfinder-front/src/pages/RtImageList.jsx
+++ pathfinder-front/src/pages/RtImageList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import RtImage from "../components/RtImage"
-import { Col, Row, Pagination } from "antd";
+import { Col, Row, Pagination, ConfigProvider } from "antd";
 import "./RtImageList.scss"
 
 const apiUrl = "http://localhost:8000/api/rt-images/";
@@ -24,18 +24,42 @@ function RtImageList() {
     }, []);
 
     return (
-        <div className="rt-images-grid">
-            {/* <h2 style={{ textAlign: "center" }}>RT Image List</h2> */}
-            <Row gutter={[16, 16]}
-            // style={{rowGap: "0px"}}
+        <div className="rt-page">
+            <div className="rt-images-grid">
+                {/* <h2 style={{ textAlign: "center" }}>RT Image List</h2> */}
+                <Row gutter={[16, 16]}
+                // style={{rowGap: "0px"}}
+                >
+                    {rtImageList.map(rtImage => (
+                        <Col className="each-grid" span={12} key={rtImage.pk}>
+                            <RtImage rtImage={rtImage} />
+                        </Col>
+                    ))}
+                </Row>
+
+            </div>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorText: 'rgb(255, 255, 255)',
+                    },
+                }}
             >
-                {rtImageList.map(rtImage => (
-                    <Col className="each-grid" span={12} key={rtImage.pk}>
-                        <RtImage rtImage={rtImage} />
-                    </Col>
-                ))}
-            </Row>
-        </div>
+                <Pagination
+                    current={1}
+                    defaultCurrent={1}
+                    defaultPageSize={6}
+                    pageSize={6}
+                    style={{
+                        textAlign: "center",
+                        margin: "3%",
+                    }}
+                    total={71}
+                    showSizeChanger={false}
+                >
+                </Pagination>
+            </ConfigProvider>
+        </div >
     );
 
 }
