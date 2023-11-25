d9fdcd88306f163b67fb499187fceb8abcadfa00
JeongHeumChoi <andyc707@gmail.com>
2023-11-25 23:59:06 +0900

    style: defect 박스 두께 조정

--- pathfinder-front/src/components/RtImage.jsx
+++ pathfinder-front/src/components/RtImage.jsx
@@ -127,7 +127,7 @@ function RtImage({ rtImage }) {
                             top: adjustedBox.top,
                             width: adjustedBox.width,
                             height: adjustedBox.height,
-                            border: '2px solid red',
+                            border: '1.5px solid red',
                             marginTop: "2%",
                         }} />
                     );
