25cac65a3a6c30425940e04289f68a228fc16503
JeongHeumChoi <andyc707@gmail.com>
2023-11-25 17:51:55 +0900

    feat: RT Image Detail Page에 나타낼 이미지 처리 로직

---
+++ pathfinder-front/src/components/RtImageDetailData.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+// import { ConfigProvider, Table, Text } from "antd";
+import { useState, useEffect, useRef } from "react";
+
+function RtImageDetailData({ rtImage }) {
+    const { image, image_name } = rtImage;
+    const defects = rtImage?.ai_model_set?.[0]?.ai_defect_set || [];
+
+    const [originalSize, setOriginalSize] = useState({ width: 0, height: 0 });
+    const [displaySize, setDisplaySize] = useState({ width: 0, height: 0 });
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const imageObj = new Image();
+        imageObj.onload = () => {
+            setOriginalSize({ width: imageObj.naturalWidth, height: imageObj.naturalHeight });
+            updateDisplaySize();
+        };
+        imageObj.src = image;
+
+        window.addEventListener('resize', updateDisplaySize);
+
+        return () => {
+            window.removeEventListener('resize', updateDisplaySize);
+        };
+    }, [image]);
+
+    const updateDisplaySize = () => {
+        if (imageRef.current) {
+            setDisplaySize({ width: imageRef.current.offsetWidth, height: imageRef.current.offsetHeight });
+        }
+    };
+
+    const calculateAdjustedBox = (box) => {
+        const widthRatio = displaySize.width / originalSize.width;
+        const heightRatio = displaySize.height / originalSize.height;
+
+
+        console.log("widthRatio : ", widthRatio);
+        console.log("heightRatio : ", heightRatio);
+
+        return {
+            left: box.xmin * widthRatio,
+            top: box.ymin * heightRatio,
+            width: (box.xmax - box.xmin) * widthRatio,
+            height: (box.ymax - box.ymin) * heightRatio
+        };
+    };
+
+    return (
+        <div className="rt-image">
+            <div style={{ position: 'relative' }}>
+                <img
+                    className="small-image"
+                    ref={imageRef}
+                    src={image}
+                    alt={image_name}
+                    style={{
+                        width: "100%",
+                        border: "3px solid white",
+                        marginTop: "2%",
+                    }}
+                />
+                {defects.map((box, index) => {
+                    const adjustedBox = calculateAdjustedBox(box);
+                    return (
+                        <div key={index} style={{
+                            position: 'absolute',
+                            left: adjustedBox.left,
+                            top: adjustedBox.top,
+                            width: adjustedBox.width,
+                            height: adjustedBox.height,
+                            border: '2px solid red'
+                        }} />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default RtImageDetailData;
