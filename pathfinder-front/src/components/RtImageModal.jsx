1a693947e695444b50baab7a57d71bdb1b0cf55f
JeongHeumChoi <andyc707@gmail.com>
2023-11-26 14:21:57 +0900

    feat: 모달창 저장 후 닫기 버튼 생성

--- pathfinder-front/src/components/RtImageModal.jsx
+++ pathfinder-front/src/components/RtImageModal.jsx
@@ -1,5 +1,7 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useRef } from "react";
 import Modal from "react-modal";
+import { Button, ConfigProvider } from "antd";
+import "./RtImageModal.scss";
 
 const RtImageModalContext = createContext();
 
@@ -8,23 +10,56 @@ export const useRtModal = () => useContext(RtImageModalContext);
 function RtImageModal({ isOpen, onRequestClose, rtImage }) {
     const { image, image_name } = rtImage;
 
+    // const [boxes, setBoxes] = useState([]);
+    // const imageRef = useRef(null);
+
+    // const handleBoxCreation = (e) => {
+    // };
+
+    // const handleBoxModification = (boxId, newDimensions) => {
+    // };
+
+    // const handleBoxDeletion = (boxId) => {
+    // };
+
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            style={{
+                overlay: {
+                    backgroundColor: "rgba(0, 0, 0, 0.75)",
+                },
+                content: {
+                    top: "10%",
+                    borderRadius: "10px",
+                    backgroundColor: "rgba(0, 0, 0, 0.75)",
+                },
+            }}
         >
-            <div className="rt-image-modal-body" style={{ zIndex: 1000 }}>
+            <div className="rt-image-modal-body">
                 <img
                     className="rt-image-modal-image"
                     src={image}
                     alt={image_name}
-                    // height={200}
-                    // width={1000}
                     style={{
                         display: "block", margin: "auto",
                     }}
                 />
-                <button onClick={onRequestClose}>닫기</button>
+                <Button
+                    className="rt-image-modal-save-button"
+                    onClick={onRequestClose}
+                    style={{
+                        display: "block",
+                        position: "fixed",
+                        // margin: "auto",
+                        // marginLeft: "auto",
+                        marginTop: "2%",
+                    }}
+                >
+                    save and close
+                </Button>
+
             </div>
         </Modal>
     );
