1806f310197207e328f5ede590ba1202f81fa0d4
JeongHeumChoi <andyc707@gmail.com>
2023-12-14 18:09:49 +0900

    feat: Welder 정보 그래프 추가

--- pathfinder-front/src/pages/welder/welder.jsx
+++ pathfinder-front/src/pages/welder/welder.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Input, Card, Statistic } from 'antd';
-import { UserOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { UserOutlined, ArrowUpOutlined, CheckCircleOutlined, CloseCircleOutlined, BgColorsOutlined, DotChartOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import './welder.scss';
 import axios from 'axios';
 
 const apiUrl = `http://127.0.0.1:8000/api/welders/`;
 
 const Welder = () => {
-  const [welderName, setWelderName] = useState("최정흠");
-  // const [welderName, setWelderName] = useState("cHgAzYS");
+  const [welderName, setWelderName] = useState("윤재호");
   const [welder, setWelder] = useState(null);
 
   useEffect(() => {
@@ -55,8 +54,8 @@ const Welder = () => {
             {welder && welder.name}</p>
         </div>
         <div className="welderSuccessDetail">
-          {welder && welder.success_count / welder.number}
           <Card bordered={false}>
+            <p className="cardTitle">용접 정확도</p>
             <Statistic
               title="용접 성공률"
               value={welder && welder.success_count / welder.number * 100}
@@ -66,12 +65,78 @@ const Welder = () => {
               suffix="%"
             />
             <Statistic
-              title="용접 성공률"
-              value={welder && welder.success_count / welder.number * 100}
-              precision={2}
+              title="용접 성공 개수"
+              value={welder && welder.success_count}
+              precision={0}
               valueStyle={{ color: '#3f8600', }}
-              prefix={<ArrowUpOutlined />}
-              suffix="%"
+              prefix={<CheckCircleOutlined />}
+              suffix="개"
+            />
+            <Statistic
+              title="용접 실패 개수"
+              value={welder && welder.number - welder.success_count}
+              precision={0}
+              valueStyle={{ color: 'red', }}
+              prefix={<CheckCircleOutlined />}
+              suffix="개"
+            />
+          </Card>
+        </div>
+        <div className="welderDefectDetail">
+          <Card bordered={false}>
+            <p className="cardTitle">실패한 용접별 결함 비율</p>
+            <Statistic
+              title="Slag"
+              value={welder && welder.slag_number}
+              precision={0}
+              valueStyle={{ color: 'red', }}
+              prefix={<BgColorsOutlined />}
+              suffix="개"
+            />
+            <Statistic
+              title="Porosity"
+              value={welder && welder.porosity_number}
+              precision={0}
+              valueStyle={{ color: 'blue', }}
+              prefix={<DotChartOutlined />}
+              suffix="개"
+            />
+            <Statistic
+              title="Others"
+              value={welder && welder.others_number}
+              precision={0}
+              valueStyle={{ color: 'green', }}
+              prefix={<QuestionCircleOutlined />}
+              suffix="개"
+            />
+          </Card>
+        </div>
+        <div className="welderDefectDetail">
+          <Card bordered={false}>
+            <p className="cardTitle">실패한 용접별 결함 비율</p>
+            <Statistic
+              title="Slag"
+              value={welder && welder.slag_number}
+              precision={0}
+              valueStyle={{ color: 'red', }}
+              prefix={<BgColorsOutlined />}
+              suffix="개"
+            />
+            <Statistic
+              title="Porosity"
+              value={welder && welder.porosity_number}
+              precision={0}
+              valueStyle={{ color: 'blue', }}
+              prefix={<DotChartOutlined />}
+              suffix="개"
+            />
+            <Statistic
+              title="Others"
+              value={welder && welder.others_number}
+              precision={0}
+              valueStyle={{ color: 'green', }}
+              prefix={<QuestionCircleOutlined />}
+              suffix="개"
             />
           </Card>
         </div>
